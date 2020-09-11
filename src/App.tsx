ece382a4abe7b967ebea2ea8113d1d53ecf28788
Daniel Brown <d.t.brown@outlook.com>
2020-09-11 23:44:15 +0100

    Add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Root, Routes } from 'react-static'
 import { Router } from '@reach/router'
 import Header from 'components/Header/Header'
+import Footer from 'components/Footer/Footer'
 import GlobalStyle from './global-styles/css-reset'
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Routes path="*" />
         </Router>
       </React.Suspense>
+      <Footer />
     </Root>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterContainer = styled.footer`
+  font-family: Secular One;
+  display: flex;
+  align-items: center;
+  color: #FFF;
+  width: calc (100% - 60px);
+  background: #272932;
+  height: 124px;
+  padding: 0px 30px;
+  justify-content: space-between;
+  font-size: 16px;
+  line-height: 23px;
+  text-align: right;
+`;
+
+const Footer = () => (
+  <FooterContainer>
+    <p>Built with ♥️in the United Kingdom</p>
+    <div>
+      <p>&copy; Brown Enterprises Ltd.</p>
+      <p>All Rights Reserved.</p>
+      <p>Registered in England and Wales:  #12663707</p>
+    </div>
+  </FooterContainer>
+);
+
+export default Footer;
