a5635c95eaefa3de21b60eda7596ec54b9b92229
Daniel Brown <d.t.brown@outlook.com>
2020-09-16 21:15:28 +0100

    Add `Button`

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import { Link } from '@reach/router';
+import styled from 'styled-components';
+
+const Button = styled(Link)`
+  height: 75px;
+  background: #F6D744;
+  padding: 0px 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-family: Secular One;
+  font-style: normal;
+  font-weight: normal;
+  color: #000;
+  text-decoration: none;
+`;
+
+export default Button;
--- src/components/WorkTogether/WorkTogether.tsx
+++ src/components/WorkTogether/WorkTogether.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StyledTitle } from 'components/Typography/Typography';
+import { StyledSubtitle, StyledTitle } from 'components/Typography/Typography';
+import Button from 'components/Button/Button';
 
 const WorkTogetherJumbotronContainer = styled.div`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
   width: 100%;
   height: 385px;
@@ -13,6 +16,8 @@ const WorkTogetherJumbotronContainer = styled.div`
 const WorkTogether = () => (
   <WorkTogetherJumbotronContainer>
     <StyledTitle colour="#272932">Lets work together</StyledTitle>
+    <StyledSubtitle colour="#272932">We work with great companies to build great products, teams and cultures</StyledSubtitle>
+    <Button to="/">Get in touch</Button>
   </WorkTogetherJumbotronContainer>
 );
 
